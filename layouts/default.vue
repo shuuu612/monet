fcd777dbdba105080232e36d65bc7f35304502c1
shunsuke <shun.8000.sf@gmail.com>
2022-02-05 16:10:59 +0900

    [update] ページ遷移アニメーションを実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,14 @@
 <template>
-  <Nuxt />
+  <transition name="nuxt">
+    <Nuxt />
+  </transition>
 </template>
+
+<style lang="scss" scoped>
+.nuxt-enter-active, .nuxt-leave-active {
+  transition: opacity .5s;
+}
+.nuxt-enter, .nuxt-leave-to {
+  opacity: 0;
+}
+</style>
