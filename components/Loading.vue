3434381d8cbe38325b99efb146d3e5c2af22e8d3
shunsuke <shun.8000.sf@gmail.com>
2022-02-08 10:39:11 +0900

    [refactor] ローディング画面をコンポーネント化

---
+++ components/Loading.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="loaded" :class="getLoadingDisplayed"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  computed: {
+    getLoadingDisplayed() {
+      return { loading: !this.$store.getters["loaded/getLoadingDisplayed"] };
+    },
+  },
+  created() {
+  },
+  mounted() {
+  },
+  beforeDestroy() {
+  },
+  methods: {
+
+  },
+  
+};
+</script>
+
+<style lang="scss" scoped>
+
+.loaded {
+  display: none;
+}
+
+.loading {
+  display: block;
+  background: linear-gradient(to bottom, #404957 50%, #6f88ad 100%);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 999;
+  opacity: 1;
+  animation-name: loading;
+  animation-duration: 1.5s;
+  animation-timing-function: ease;
+  animation-delay: 0s;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+  animation-fill-mode: forwards;
+  animation-play-state: running;
+  
+  @keyframes loading {
+    0% {
+      
+    }
+    70% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+
+  &:before {
+    content: '';
+    position: fixed;
+    top: 50vh;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    z-index: 999;
+    background-color: var(--white);
+    clip-path:inset(0 100% 0 0);
+    animation-name: loadingLine;
+    animation-duration: 1.5s;
+    animation-timing-function: ease;
+    animation-delay: 0s;
+    animation-iteration-count: 1;
+    animation-direction: normal;
+    animation-fill-mode: forwards;
+    animation-play-state: running;
+    
+    @keyframes loadingLine {
+      0% {
+        
+      }
+      10% {
+        clip-path:inset(0 100% 0 0);
+      }
+      30% {
+        clip-path:inset(0 0 0 0);
+      }
+      40% {
+        clip-path:inset(0 0 0 0);
+      }
+      60% {
+        clip-path:inset(0 0 0 100%);
+      }
+      100% {
+        
+      }
+    }
+  }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper" :class="getSideMenuOpen">
-    <div class="loaded" :class="getLoadingDisplayed"></div>
+    <Loading />
     <Header
     @search="keywordSearchStart"
     />
@@ -515,9 +515,6 @@ export default {
             return function (id) {
                 return this.activeModal.includes(id)
             };
-        },
-        getLoadingDisplayed() {
-            return { loading: !this.$store.getters["loaded/getLoadingDisplayed"] };
         },
         getSideMenuOpen() {
             return { sideMenuOpen: this.$store.getters["sideMenu/getOpen"] };
@@ -1624,83 +1621,6 @@ export default {
   }
 }
 
-.loaded {
-  display: none;
-}
-
-.loading {
-  display: block;
-  background: linear-gradient(to bottom, #404957 50%, #6f88ad 100%);
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 999;
-  opacity: 1;
-  animation-name: loading;
-  animation-duration: 1.5s;
-  animation-timing-function: ease;
-  animation-delay: 0s;
-  animation-iteration-count: 1;
-  animation-direction: normal;
-  animation-fill-mode: forwards;
-  animation-play-state: running;
-  
-  @keyframes loading {
-    0% {
-      
-    }
-    70% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-      visibility: hidden;
-    }
-  }
-
-  &:before {
-    content: '';
-    position: fixed;
-    top: 50vh;
-    left: 0;
-    width: 100%;
-    height: 2px;
-    z-index: 999;
-    background-color: var(--white);
-    clip-path:inset(0 100% 0 0);
-    animation-name: loadingLine;
-    animation-duration: 1.5s;
-    animation-timing-function: ease;
-    animation-delay: 0s;
-    animation-iteration-count: 1;
-    animation-direction: normal;
-    animation-fill-mode: forwards;
-    animation-play-state: running;
-    
-    @keyframes loadingLine {
-      0% {
-        
-      }
-      10% {
-        clip-path:inset(0 100% 0 0);
-      }
-      30% {
-        clip-path:inset(0 0 0 0);
-      }
-      40% {
-        clip-path:inset(0 0 0 0);
-      }
-      60% {
-        clip-path:inset(0 0 0 100%);
-      }
-      100% {
-        
-      }
-    }
-  }
-}
 .divider {
   position: relative;
   color: var(--main-text);
