ac878e852ec9e5b4a09b8be37d6ad85fe6b2e1dc
shunsuke <shun.8000.sf@gmail.com>
2022-03-05 16:12:08 +0900

    [test] キャッシュバスターが機能しているかのテスト

--- components/Images.vue
+++ components/Images.vue
@@ -303,7 +303,7 @@ export default {
       const today = new Date();
       const year = today.getFullYear();
       const month = (today.getMonth() + 1).toString().padStart(2, "0");
-      const date = today.getDate().toString().padStart(2, "0");
+      const date = (today.getDate() + 1).toString().padStart(2, "0");
       this.date = year+month+date;
     }
   },
