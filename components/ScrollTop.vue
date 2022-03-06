0e2724c3743afb290068a076a622ef20966b880d
shunsuke <shun.8000.sf@gmail.com>
2022-03-06 20:36:54 +0900

    [update] アクセシビリティの向上

--- components/ScrollTop.vue
+++ components/ScrollTop.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="getDesktop">
-    <button class="scrollTop" :class="getHide" @click="$moveTop">
+    <button class="scrollTop" :class="getHide" aria-label="scroll top" @click="$moveTop">
       <!-- <div class="scrollTopText">Top</div> -->
       <!-- <svg class="scrollTopImage" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 514.48 411.87" fill="#000000">
         <path d="M342.06,0H36V90h306a82.42,82.42,0,1,1,0,164.83h-148v-67L0,299.83l194.05,112v-67h148c95.07,0,172.42-77.34,172.42-172.41S437.13,0,342.06,0Z"/>
