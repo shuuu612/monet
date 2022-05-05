57b8faf1ef034e1283d24edc300c0d2931b8bb12
shunsuke <shun.8000.sf@gmail.com>
2022-05-05 14:38:48 +0900

    [fix] ファビコン設定の見直し

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180'},
       { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap'},
