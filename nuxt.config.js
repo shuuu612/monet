53f85b26ab06e908bb4dde1096cfddee81c016df
shunsuke <shun.8000.sf@gmail.com>
2022-07-22 09:05:06 +0900

    change deploy location

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const PRODUCTION_BASE_PATH = '/works/monet';
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -7,7 +9,7 @@ export default {
     title: 'Monet | Webデザインギャラリー',
     htmlAttrs: {
       lang: 'ja',
-      prefix: 'og: http://ogp.me/ns#'
+      prefix: 'og: http://ogp.me/ns#',
     },
     meta: [
       { charset: 'utf-8' },
@@ -16,45 +18,48 @@ export default {
       { hid: 'keywords', name: 'keywords', content: 'Webデザイン,Web design,ギャラリー' },
       { name: 'format-detection', content: 'telephone=no' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-      
+
       // OGP設定
       { hid: 'og:site_name', property: 'og:site_name', content: 'Monet' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: 'https://monet.shuuu.jp/' },
+      { hid: 'og:url', property: 'og:url', content: 'https://shuuu.jp/works/monet' },
       { hid: 'og:title', property: 'og:title', content: 'Monet | Webデザインギャラリー' },
       { hid: 'og:description', property: 'og:description', content: 'MonetはWebサイトギャラリーです。' },
-      { hid: 'og:image', property: 'og:image', content: 'https://monet.shuuu.jp/ogp.png' },
+      { hid: 'og:image', property: 'og:image', content: 'https://shuuu.jp/works/monet/ogp.png' },
 
       // Twitter
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'twitter:site', name: 'twitter:site', content: '@shuuu612' }
+      { hid: 'twitter:site', name: 'twitter:site', content: '@shuuu612' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180'},
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap'},
-    ]
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: `${process.env.NODE_ENV === 'production' ? PRODUCTION_BASE_PATH : ''}/favicon.ico`,
+      },
+      {
+        rel: 'apple-touch-icon',
+        href: `${process.env.NODE_ENV === 'production' ? PRODUCTION_BASE_PATH : ''}/apple-touch-icon.png`,
+        sizes: '180x180',
+      },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/scss/ress.scss',
-    '@/assets/scss/common.scss'
-  ],
+  css: ['@/assets/scss/ress.scss', '@/assets/scss/common.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/common.js',
-    '@/plugins/storageAvailable.js',
-  ],
+  plugins: ['@/plugins/common.js', '@/plugins/storageAvailable.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components:  {
-    dirs: [
-      '~/components',
-    ]
+  components: {
+    dirs: ['~/components'],
   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
@@ -65,21 +70,13 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/style-resources',
-    '@nuxtjs/google-gtag', 
-  ],
+  modules: ['@nuxtjs/axios', '@nuxtjs/style-resources', '@nuxtjs/google-gtag'],
   'google-gtag': {
     id: 'G-QDLBX51TVH',
   },
 
   styleResources: {
-    scss: [
-      '@/assets/scss/mixin.scss',
-      '@/assets/scss/font.scss',
-      '@/assets/scss/color.scss',
-    ]
+    scss: ['@/assets/scss/mixin.scss', '@/assets/scss/font.scss', '@/assets/scss/color.scss'],
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -87,10 +84,14 @@ export default {
     terser: {
       terserOptions: {
         compress: {
-          drop_console: process.env.NODE_ENV === "production"
-        }
-      }
+          drop_console: process.env.NODE_ENV === 'production',
+        },
+      },
     },
+  },
+
+  router: {
+    base: process.env.NODE_ENV === 'production' ? PRODUCTION_BASE_PATH : '/',
   },
 
   microcms: {
@@ -98,10 +99,10 @@ export default {
       serviceDomain: process.env.SERVICE_ID,
       apiKey: process.env.API_KEY,
     },
-    mode: process.env.NODE_ENV === "production" ? "server" : "all",
+    mode: process.env.NODE_ENV === 'production' ? 'server' : 'all',
   },
 
   generate: {
     interval: 300,
-  }
-}
+  },
+};
